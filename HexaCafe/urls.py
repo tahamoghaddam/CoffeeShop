a70590036c5a8eb138128de146a29d337943b119
Taha Moghaddam <taha.moghaddam82@gmail.com>
2024-06-29 11:32:43 +0330

    ss

--- HexaCafe/urls.py
+++ HexaCafe/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from .views import signup,login, add_product , update_inventory, home,cart_view, add_to_cart
+from .views import signup,login, add_product, update_inventory, home,cart_view, add_to_cart
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/',login , name = 'login'),
-    path('signup/' ,signup , name = 'signup'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('addproduct/' ,add_product , name = 'addproduct' ),
     path('update_inventory/' , update_inventory, name = 'inventory_update'),
-    path('home/', home , name = 'home'),
+    path('home/', home, name='home'),
     path('cart/', cart_view, name='cart'),
     path('add_to_cart/', add_to_cart, name='add_to_cart'),
 ]
